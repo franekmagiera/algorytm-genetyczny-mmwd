2228f98b659c420a6db09f6b1a730622033075ba
dlech97 <dlech97@gmail.com>
2019-01-08 17:24:37 +0100

    Add files via upload

--- algv3/fitness_function.py
+++ algv3/fitness_function.py
@@ -12,12 +12,16 @@ def fitness_function(solution, transport_cost_matrix, distance_matrix):
             # column_i = i % columns
             # row_j = j // columns
             # column_j = j % columns
-            transport_cost = transport_cost_matrix[solution[i], solution[j]] # + transport_cost_matrix[solution[j], solution[i]]
+            transport_cost = transport_cost_matrix[solution[i], solution[j]] + transport_cost_matrix[solution[j], solution[i]]
             # distance = abs(row_j - row_i) + abs(column_j - column_i) # odkomentowac jesli nie bedziemy korzystac z macierzy odleglosci
             distance = distance_matrix[i, j]
             value += transport_cost * distance 
-    return value/2 # aktualnie badamy jedynie symetryczne macierze kosztow
+    return value/2  # aktualnie badamy jedynie symetryczne macierze kosztow
 
 
 def fit_fun(solution):
     return fitness_function(solution, transport_cost_matrix, distance_matrix)
+
+#OPTIMAL SOLUTION
+#sol = [34,4,5,11,10,26,25,24,23,8,3,0,12,19,13,22,20,21,1,7,9,6,27,18,31,33,32,16,17,2,14,15,28,29,30,35]
+#print(fit_fun(sol))
